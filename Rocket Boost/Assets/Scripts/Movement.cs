ba78814012ef5e5b9daf9cb4375e86290a5afb3b
Victor Real <victor.realchisto30@hotmail.com>
2025-09-21 19:09:52 -0300

    added thrust control to movement script

--- Rocket Boost/Assets/Scripts/Movement.cs
+++ Rocket Boost/Assets/Scripts/Movement.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] InputAction thrust;
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,8 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (thrust.IsPressed()) {
+            Debug.Log("Tested");
+        }
     }
 }
